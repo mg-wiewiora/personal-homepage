bcc6593a9670c84d3ba382c6788f2a42490ecc35
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-12-31 05:36:12 +0100

    styles added to ButtonLink

---
+++ src/features/personalHomepage/PersonalHomepage/ButtonLink/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ButtonLink = styled.a`
+  text-decoration: none;
+  background: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.buttonLink.text};
+  padding: 12px 16px;
+  font-size: 20px;
+  font-weight: 600;
+  border: 1px solid ${({ theme }) => theme.colors.buttonLink.border};
+  border-radius: ${({ theme }) => theme.borderRadiusSmall};
+  transition: box-shadow 0.3s;
+
+  &:hover {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.buttonLink.shadow};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 18px;
+  }
+`;
