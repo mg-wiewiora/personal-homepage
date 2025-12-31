161e063b3e3b93dbc175f6ab8766c5e9b6e23847
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-12-31 04:13:41 +0100

    App import fixed

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { PersonalHomepage } from "../../features/personalHomepage/PersonalHomepage";
+import { GlobalStyle } from "./GlobalStyle";
+import { themeDark, themeLight } from "./theme";
+import { Normalize } from "styled-normalize";
+import { selectIsDarkTheme } from "../../common/themeSlice";
+
 const App = () => {
   const isDarkTheme = useSelector(selectIsDarkTheme);
 
-  return <PersonalHomepage />;
+  return (
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+      <Normalize />
+      <GlobalStyle />
+      <PersonalHomepage />
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import { Provider } from "react-redux";
-import { GlobalStyle } from "./GlobalStyle";
-import App from "./App";
+import App from "./core/App";
 import reportWebVitals from "./reportWebVitals";
-import { themeDark, themeLight } from "./theme";
-import store from "./store";
+import store from "./core/store"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
-       <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+    <App />
     </Provider>
   </React.StrictMode>
 );
