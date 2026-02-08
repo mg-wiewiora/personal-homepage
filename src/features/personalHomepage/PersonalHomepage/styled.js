fcea8f50875f48ac472a7b09e81903c9f1b39da1
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2026-02-09 00:06:51 +0100

    global margin and paddding updated

--- src/features/personalHomepage/PersonalHomepage/styled.js
+++ src/features/personalHomepage/PersonalHomepage/styled.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 1248px;
-  margin: auto;
-  padding: 16px;
+  margin: 0 auto;
+  padding: 20px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     padding: 8px;
